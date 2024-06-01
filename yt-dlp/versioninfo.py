810d5569deee07fe3750ac4c64a9aa9501643ee1
Cheng Ho Ming, Eric <eric310@connect.hku.hk>
2024-06-02 02:27:05 +0800

    yt-dlp build: Fix bug when time has leading zero

--- yt-dlp/versioninfo.py
+++ yt-dlp/versioninfo.py
@@ -10,26 +10,26 @@ from datetime import datetime
 with open(os.path.join(os.path.dirname(__file__), 'versioninfo.txt'), 'w', encoding='utf-8') as f:
     # filevers should be always a tuple with four items: (1, 0, 0, 0)
     # it will be year, month, day, UTC+8 time in format of Hour+Minute (e.g. 2021, 8, 1, 1200)
-    f.write("""VSVersionInfo(
-    ffi=FixedFileInfo(
-        filevers=({}, {}, {}, {})
-        ),
-    kids=[
-        StringFileInfo(
-        [
-        StringTable(
-            u'040904B0',
-            [StringStruct(u'CompanyName', u'github.com/yt-dlp/yt-dlp'),
-            StringStruct(u'FileDescription', u'A feature-rich command-line audio/video downloader'),
-            StringStruct(u'FileVersion', u'{}'),
-            StringStruct(u'InternalName', u'yt-dlp'),
-            StringStruct(u'LegalCopyright', u'This is free and unencumbered software released into the public domain.'),
-            StringStruct(u'OriginalFilename', u'yt-dlp.exe'),
-            StringStruct(u'ProductName', u'yt-dlp'),
-            StringStruct(u'ProductVersion', u'{}')])
-        ]),
-        VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
-        ]
-    )""".format(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().strftime('%H%M'), __version__, __version__))
+    f.write(f"""VSVersionInfo(
+        ffi=FixedFileInfo(
+            filevers=({datetime.now().year}, {datetime.now().month}, {datetime.now().day}, {str(datetime.now().strftime('%H%M')).lstrip('0')})
+            ),
+        kids=[
+            StringFileInfo(
+            [
+            StringTable(
+                u'040904B0',
+                [StringStruct(u'CompanyName', u'github.com/yt-dlp/yt-dlp'),
+                StringStruct(u'FileDescription', u'A feature-rich command-line audio/video downloader'),
+                StringStruct(u'FileVersion', u'{__version__}'),
+                StringStruct(u'InternalName', u'yt-dlp'),
+                StringStruct(u'LegalCopyright', u'This is free and unencumbered software released into the public domain.'),
+                StringStruct(u'OriginalFilename', u'yt-dlp.exe'),
+                StringStruct(u'ProductName', u'yt-dlp'),
+                StringStruct(u'ProductVersion', u'{__version__}')])
+            ]),
+            VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
+            ]
+        )""")
 
 print('Version information written to versioninfo.txt')
