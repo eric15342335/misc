5f5a8417a9e90222966b08ce9d8bc02e30d38349
Cheng Ho Ming, Eric <eric310@connect.hku.hk>
2024-07-09 23:06:26 +0800

    update yt-dlp autobuild script

--- yt-dlp/autobuild-yt-dlp.cmd
+++ yt-dlp/autobuild-yt-dlp.cmd
@@ -1,27 +1,42 @@
 rem Clone the respective repositories
-git clone https://github.com/pyinstaller/pyinstaller --depth=1
+git clone https://github.com/pyinstaller/pyinstaller --depth=50
 git clone https://github.com/yt-dlp/yt-dlp --depth=1
-virtualenv venv
+pip cache purge
+
+virtualenv venv --clear
 venv\scripts\activate
 cd pyinstaller/bootloader
+
 rem Cmd and Powershell command
 set PATH=%PATH%;E:\Personal Data\Repositories\mingw64\bin
 rem $env:PATH += 'E:\Personal Data\Repositories\mingw64\bin'
+
 rem Build PyInstaller bootloader using custom gcc binaries
-python .\waf all --gcc
+set CFLAGS=-flto
+set LDFLAGS=-flto
+python .\waf all --gcc -j 21
 cd ..\
 pip3 install -e .
 cd ..\
-rm -rf pyinstaller
-rem Build yt-dlp
 cd yt-dlp
+
+rem Update version info
 python devscripts/update-version.py
-rm ../versioninfo.txt
-python ../versioninfo.py
-pyinstaller -y --clean yt_dlp/__main__.py --name yt-dlp --noupx --version-file ../versioninfo.txt
+cp ../versioninfo.py versioninfo.py
+python versioninfo.py
+
+rem Build yt-dlp
+set PYTHONOPTIMIZE=2
+pip install -e .
+pyinstaller -y --clean yt_dlp/__main__.py --name yt-dlp --noupx --version-file versioninfo.txt --collect-submodules yt-dlp.yt_dlp.utils --icon ../banner.ico --recursive-copy-metadata yt-dlp --recursive-copy-metadata pyinstaller
+pyinstaller -y --onefile --clean yt_dlp/__main__.py --name yt-dlp --noupx --version-file versioninfo.txt --collect-submodules yt-dlp.yt_dlp.utils --icon ../banner.ico --recursive-copy-metadata yt-dlp --recursive-copy-metadata pyinstaller
 cp ..\ffmpeg.exe dist\yt-dlp\
 cp -r dist\yt-dlp\ ..\yt_dlp\
 cp README.md ..\yt_dlp\
-cp LICENSE ..\yt_dlp\
 cd ..\
-rm -rf yt-dlp
+
+rem Clean up
+rem rm -rf yt-dlp
+rem rm -rf pyinstaller
+rem rm -rf venv
+
